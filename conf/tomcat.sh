411bb0450d7c3c734f5c1d70d057f3fe67beefe6
shridharkapshikar <shridhar.kapshikar@hotmail.com>
2019-09-16 19:39:11 +0530

    Update tomcat.sh

--- conf/tomcat.sh
+++ conf/tomcat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-yum install -y java-1.8.0-openjdk-devel
+sudo yum install -y java-1.8.0-openjdk-devel
 
 #tomcat download
-cd /opt/
-wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz
-tar -zxvf apache-tomcat-7.0.53.tar.gz
+sudo cd /opt/
+sudo wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz
+sudo tar -zxvf apache-tomcat-7.0.53.tar.gz
 sleep 25
-rm apache-tomcat-7.0.53.tar.gz
+sudo rm apache-tomcat-7.0.53.tar.gz
 sleep 25
 cd /opt/apache-tomcat-7.0.53/bin/
-sh startup.sh &
+sudo ./startup.sh
