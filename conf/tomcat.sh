79986f2e23d4e7d67b8d714611dbd2aa981a0e58
shridhar <shridhar.kapshikar@hotmail.com>
2019-09-16 09:58:53 -0400

    modified tomcat install

--- conf/tomcat.sh
+++ conf/tomcat.sh
@@ -5,7 +5,8 @@ yum install -y java-1.8.0-openjdk-devel
 cd /opt/
 wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz
 tar -zxvf apache-tomcat-7.0.53.tar.gz
+sleep 25
 rm apache-tomcat-7.0.53.tar.gz
-
+sleep 25
 cd /opt/apache-tomcat-7.0.53/bin/
-./startup.sh &
+sh startup.sh &
